adb15e1b98034d6e691c095336c8f4d6fb59f3e8
FM-Akib <muntasirakib08@gmail.com>
2025-01-05 23:58:04 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions":"error",
+      "prefer:const":"error",
+      "no-console":"warn",
+      "no-undef":"error",
+      
+    },
+    globals:{
+      "process":"readonly"
+      }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test Route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
